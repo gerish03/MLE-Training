fc2be5d92b76134ea12626d46eb77860dc7b638f
gerish01 <gerish.r@tigeranalytics.com>
2024-03-28 13:42:46 +0530

    Updated the file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='HousePricePrediction',
+    name='mypackage',
     version='0.0.1',
     install_requires=[
         'requests',
--- src/HousePricePrediction/score.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.metrics import mean_squared_error
-
-
-def score_model(model, X_test, y_test):
-    predictions = model.predict(X_test)
-    mse = mean_squared_error(y_test, predictions)
-    rmse = np.sqrt(mse)
-    return rmse
