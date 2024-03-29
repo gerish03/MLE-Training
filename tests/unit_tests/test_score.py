aa2a1c4855d760b152b7467108b15b678dee7b60
gerish01 <gerish.r@tigeranalytics.com>
2024-03-29 22:42:40 +0530

    Unit_testing for score.py

--- tests/unit_tests/test_score.py
+++ tests/unit_tests/test_score.py
@@ -1,38 +1,36 @@
+import os
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import patch
 
-import numpy as np
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-
-from HousePricePrediction.score import (RF_score, score_model_mae,
-                                        score_model_rmse)
+from HousePricePrediction.score import RF_score, save_metrics
 
 
 class TestScoreFunctions(unittest.TestCase):
     def setUp(self):
-        np.random.seed(0)
-        self.X_test = np.random.rand(100, 10)
-        self.y_test = np.random.rand(100)
-        self.cvres = {"mean_test_score": np.random.rand(10), "params": ["params" + str(i) for i in range(10)]}
-        self.model = MagicMock()
-        self.model.predict.return_value = np.random.rand(100)
+        self.model_name = "test_model"
+        self.score_type = "RMSE"
+        self.score = 0.0
 
-    def test_score_model_rmse(self):
-        predictions = self.model.predict(self.X_test)
-        mse = mean_squared_error(self.y_test, predictions)
-        rmse = np.sqrt(mse)
-        self.assertIsInstance(rmse, float)
+    def test_save_metrics(self):
+        save_metrics(self.model_name, self.score_type, self.score)
+        filename = f"artifacts/metrics/{self.model_name}_{self.score_type}.txt"
+        assert os.path.exists(filename), f"File {filename} was not created."
 
-    def test_score_model_mae(self):
-        predictions = self.model.predict(self.X_test)
-        mae = mean_absolute_error(self.y_test, predictions)
-        self.assertIsInstance(mae, float)
+    @patch("builtins.print")
+    def test_RF_score(self, mock_print):
+
+        cvres = {
+            "mean_test_score": [-100, -200],
+            "params": [{"param1": 1}, {"param2": 2}],
+        }
+        mock_model = type("MockModel", (), {})
+        RF_score(cvres, mock_model)
+        filename = f"artifacts/metrics/{type(mock_model).__name__}_RMSE.txt"
+        assert os.path.exists(filename), f"File {filename} was not created."
+        assert (
+            mock_print.call_count == 2
+        ), "Print function was not called twice."
 
 
-    def test_RF_score(self):
-        for mean_score, params in zip(self.cvres["mean_test_score"], self.cvres["params"]):
-            self.assertIsInstance(mean_score, float)
-            self.assertIsInstance(params, str)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
