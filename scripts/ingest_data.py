d83726d61b31f790904c0db54e57df87d86647ad
gerish01 <gerish.r@tigeranalytics.com>
2024-04-01 11:59:03 +0530

    Workflow for Logging yml file

--- scripts/ingest_data.py
+++ scripts/ingest_data.py
@@ -7,32 +7,51 @@ from HousePricePrediction.ingest_data import (StratifiedShuffleSplit_data,
                                               load_housing_data,
                                               preprocessing_data)
 
+# Configure logging
+logger = logging.getLogger("scripts")
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-def setup_logger(log_level, log_to_file=True):
-    logger = logging.getLogger(__name__)
-    logger.setLevel(log_level)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    if log_to_file:
-        file_handler = logging.FileHandler('ingest_data.log')
+# Define console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+
+def setup_logging(log_level, log_path, console_log):
+    numeric_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError("Invalid log level: %s" % log_level)
+    logger.setLevel(numeric_level)
+
+    if log_path:
+        log_directory, log_file = os.path.split(log_path)
+        if not log_directory:
+            log_directory = "."
+        os.makedirs(log_directory, exist_ok=True)
+        if not log_file:
+            log_file = "logfile.log"
+        log_file_path = os.path.join(log_directory, log_file)
+        file_handler = logging.FileHandler(log_file_path)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-    return logger
 
-def data_ingestion(raw_dataset_path, processed_dataset_path, log_level=logging.INFO, log_to_file=True):
-    logger = setup_logger(log_level, log_to_file)
-    logger.info("Fetching housing data...")
+    if not console_log:
+        logger.removeHandler(console_handler)
+
+
+def data_ingestion(raw_dataset_path, processed_dataset_path):
+    logger.info("Starting data ingestion process...")
     fetch_housing_data(housing_path=raw_dataset_path)
-    logger.info("Loading housing data...")
+    logger.info("Housing data fetched successfully.")
     housing_data = load_housing_data(raw_dataset_path)
-    logger.info("Splitting housing data into train and test sets...")
+    logger.info("Housing data loaded successfully.")
     strat_train_set, strat_test_set = StratifiedShuffleSplit_data(housing_data)
-    logger.info("Preprocessing training data...")
+    logger.info("Data split into train and test sets.")
     X_train, y_train = preprocessing_data(strat_train_set)
-    logger.info("Preprocessing testing data...")
+    logger.info("Preprocessing of training data completed.")
     X_test, y_test = preprocessing_data(strat_test_set)
+    logger.info("Preprocessing of test data completed.")
 
     os.makedirs(processed_dataset_path, exist_ok=True)
     train_data_path = os.path.join(processed_dataset_path, "X_train.csv")
@@ -40,22 +59,41 @@ def data_ingestion(raw_dataset_path, processed_dataset_path, log_level=logging.I
     train_labels_path = os.path.join(processed_dataset_path, "y_train.csv")
     test_labels_path = os.path.join(processed_dataset_path, "y_test.csv")
 
-    logger.info("Saving preprocessed data to CSV files...")
     X_train.to_csv(train_data_path, index=False)
     X_test.to_csv(test_data_path, index=False)
     y_train.to_csv(train_labels_path, index=False)
     y_test.to_csv(test_labels_path, index=False)
-    logger.info("Data ingestion completed!")
+    logger.info("Data saved to processed dataset folder.")
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Download and preprocess housing data")
-    parser.add_argument("raw_dataset_path", type=str, nargs="?", default="data/raw", help="Input datasets folder (default: data/raw)")
-    parser.add_argument("processed_dataset_path", type=str, nargs="?", default="data/processed", help="Processed Dataset folder (default: data/processed)")
-    parser.add_argument("--log-level", type=str, default="INFO", help="Logging level (default: INFO)")
-    parser.add_argument("--log-path", type=str, help="Path to save log file")
-    parser.add_argument("--no-console-log", action="store_false", help="Disable logging to console")
+    parser = argparse.ArgumentParser(
+        description="Download and preprocess housing data"
+    )
+    parser.add_argument(
+        "raw_dataset_path",
+        type=str,
+        nargs="?",
+        default="data/raw",
+        help="Input datasets folder (default: data/raw)",
+    )
+    parser.add_argument(
+        "processed_dataset_path",
+        type=str,
+        nargs="?",
+        default="data/processed",
+        help="Processed Dataset folder (default: data/processed)",
+    )
+    parser.add_argument(
+        "--log-level",
+        default="INFO",
+        help="Specify log level (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL)",
+    )
+    parser.add_argument("--log-path", default="", help="Specify log file path")
+    parser.add_argument(
+        "--no-console-log", action="store_true", help="Disable console logging"
+    )
     args = parser.parse_args()
 
-    log_level = getattr(logging, args.log_level.upper(), logging.INFO)
-    log_to_file = True if args.log_path else False
-    data_ingestion(args.raw_dataset_path, args.processed_dataset_path, log_level=log_level, log_to_file=log_to_file)
+    setup_logging(args.log_level, args.log_path, not args.no_console_log)
+    data_ingestion(args.raw_dataset_path, args.processed_dataset_path)
--- scripts/train.py
+++ scripts/train.py
@@ -10,33 +10,46 @@ from HousePricePrediction.train import (grid_tune_random_forest,
                                         train_decision_tree,
                                         train_linear_regression)
 
+# Configure logging
+logger = logging.getLogger("script")
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-def setup_logger(log_level, log_to_file=True):
-    logger = logging.getLogger(__name__)
-    logger.setLevel(log_level)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    if log_to_file:
-        file_handler = logging.FileHandler('train.log')
+# Define console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+
+def setup_logging(log_level, log_path, console_log):
+    numeric_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError("Invalid log level: %s" % log_level)
+    logger.setLevel(numeric_level)
+
+    if log_path:
+        log_directory, log_file = os.path.split(log_path)
+        if not log_directory:
+            log_directory = "."
+        os.makedirs(log_directory, exist_ok=True)
+        if not log_file:
+            log_file = "logfile.log"
+        log_file_path = os.path.join(log_directory, log_file)
+        file_handler = logging.FileHandler(log_file_path)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-    return logger
 
-def training_model(processed_dataset_path, ml_model_path, log_level=logging.INFO, log_to_file=True):
-    logger = setup_logger(log_level, log_to_file)
-    logger.info("Creating ML models...")
+    if not console_log:
+        logger.removeHandler(console_handler)
+
+
+def training_model(processed_dataset_path, ml_model_path):
+    logger.info("Starting model training...")
     os.makedirs(ml_model_path, exist_ok=True)
-    logger.info(f"Processed dataset path: {processed_dataset_path}")
-    logger.info(f"ML model path: {ml_model_path}")
-
     X_train_path = os.path.join(processed_dataset_path, "X_train.csv")
-    logger.info(f"Loading X_train data from: {X_train_path}")
     X_train = pd.read_csv(X_train_path)
 
     y_train_path = os.path.join(processed_dataset_path, "y_train.csv")
-    logger.info(f"Loading y_train data from: {y_train_path}")
     y_train = pd.read_csv(y_train_path).iloc[:, 0]
 
     logger.info("Training Linear Regression model...")
@@ -45,37 +58,62 @@ def training_model(processed_dataset_path, ml_model_path, log_level=logging.INFO
     logger.info("Training Decision Tree model...")
     DT_model = train_decision_tree(X_train, y_train)
 
-    logger.info("Training Randomized Search CV Random Forest model...")
+    logger.info("Training Random Forest model with randomized search...")
     rand_tune_RF_model = rand_tune_random_forest(X_train, y_train)
 
-    logger.info("Training Grid Search CV Random Forest model...")
+    logger.info("Training Random Forest model with grid search...")
     grid_tune_Tuned_RF_model = grid_tune_random_forest(X_train, y_train)
     grid_tune_Tuned_RF_model.best_params_
 
-    feature_importances = grid_tune_Tuned_RF_model.best_estimator_.feature_importances_
-    logger.info("Feature Importances:")
-    logger.info(sorted(zip(feature_importances, X_train.columns), reverse=True))
+    feature_importances = (
+        grid_tune_Tuned_RF_model.best_estimator_.feature_importances_
+    )
+    sorted(zip(feature_importances, X_train.columns), reverse=True)
 
     final_model = grid_tune_Tuned_RF_model.best_estimator_
 
     logger.info("Saving trained models...")
     dump(LR_model, os.path.join(ml_model_path, "LR_model.pkl"))
     dump(DT_model, os.path.join(ml_model_path, "DT_model.pkl"))
-    dump(rand_tune_RF_model, os.path.join(ml_model_path, "rand_tune_RF_model.pkl"))
-    dump(grid_tune_Tuned_RF_model, os.path.join(ml_model_path, "grid_tune_Tuned_RF_model.pkl"))
+    dump(
+        rand_tune_RF_model,
+        os.path.join(ml_model_path, "rand_tune_RF_model.pkl"),
+    )
+    dump(
+        grid_tune_Tuned_RF_model,
+        os.path.join(ml_model_path, "grid_tune_Tuned_RF_model.pkl"),
+    )
     dump(final_model, os.path.join(ml_model_path, "final_model.pkl"))
 
-    logger.info("Training completed successfully!")
+    logger.info("Training completed and models saved to %s", ml_model_path)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Making ML model and Storing")
-    parser.add_argument("processed_dataset_path", type=str, nargs="?", default="data/processed", help="Processed Dataset folder (default: data/processed)")
-    parser.add_argument("ml_model_path", type=str, nargs="?", default="artifacts/models", help="ML Models folder (default: artifacts/models)")
-    parser.add_argument("--log-level", type=str, default="INFO", help="Logging level (default: INFO)")
-    parser.add_argument("--log-path", type=str, help="Path to save log file")
-    parser.add_argument("--no-console-log", action="store_false", help="Disable logging to console")
+    parser.add_argument(
+        "processed_dataset_path",
+        type=str,
+        nargs="?",
+        default="data/processed",
+        help="Processed Dataset folder (default: data/processed)",
+    )
+    parser.add_argument(
+        "ml_model_path",
+        type=str,
+        nargs="?",
+        default="artifacts/models",
+        help="ML Models folder (default: artifacts/models)",
+    )
+    parser.add_argument(
+        "--log-level",
+        default="INFO",
+        help="Specify log level (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL)",
+    )
+    parser.add_argument("--log-path", default="", help="Specify log file path")
+    parser.add_argument(
+        "--no-console-log", action="store_true", help="Disable console logging"
+    )
     args = parser.parse_args()
 
-    log_level = getattr(logging, args.log_level.upper(), logging.INFO)
-    log_to_file = True if args.log_path else False
-    training_model(args.processed_dataset_path, args.ml_model_path, log_level=log_level, log_to_file=log_to_file)
+    setup_logging(args.log_level, args.log_path, not args.no_console_log)
+    training_model(args.processed_dataset_path, args.ml_model_path)
